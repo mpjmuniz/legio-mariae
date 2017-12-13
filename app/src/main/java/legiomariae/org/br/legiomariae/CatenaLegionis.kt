e27c5d4e2e6c5bee33f80299a35cfdcc8ed40612
marcelopjmuniz <marcelopjmuniz@gmail.com>
2017-12-13 18:14:35 -0200

    app started, using legacy layout scheme for main activity

---
+++ app/src/main/java/legiomariae/org/br/legiomariae/CatenaLegionis.kt
@@ -0,0 +1,12 @@
+package legiomariae.org.br.legiomariae
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CatenaLegionis : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catena_legionis)
+    }
+}
