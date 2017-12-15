2dfdc253e89f63c6eeee762e1ccc615a5ceb2f26
marcelopjmuniz <marcelopjmuniz@gmail.com>
2017-12-15 12:36:03 -0200

    compressed strings with XML ENTITies, acceptable state for catena in portuguese

--- app/src/main/java/legiomariae/org/br/legiomariae/CatenaLegionis.kt
+++ app/src/main/java/legiomariae/org/br/legiomariae/CatenaLegionis.kt
@@ -1,7 +1,8 @@
 package legiomariae.org.br.legiomariae
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class CatenaLegionis : AppCompatActivity() {
 
@@ -9,4 +10,50 @@ class CatenaLegionis : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catena_legionis)
     }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.latin_portuguese, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId)
+        {
+            R.id.btnLatim -> {
+                Thread({
+                    try {
+                        runOnUiThread({
+                            if(item.isChecked) {
+                                Toast.makeText(this, "Trocando para o latim", Toast.LENGTH_SHORT)
+                                        .show()
+                                item.isChecked = false
+                                tvAntInicial.text = resources.getString(R.string.antifonaCtLt)
+                                tvAntFinal.text = resources.getString(R.string.antifonaCtLt)
+                                tvMagnificat.text = resources.getString(R.string.magnificatCtLt)
+                                tvPreces.text = resources.getString(R.string.precesCtLt)
+                            }
+                            else {
+                                Toast.makeText(this, "Trocando para o português", Toast.LENGTH_SHORT)
+                                        .show()
+                                item.isChecked = true
+
+                                tvAntInicial.text = resources.getString(R.string.antifonaCt)
+                                tvAntFinal.text = resources.getString(R.string.antifonaCt)
+                                tvMagnificat.text = resources.getString(R.string.magnificatCt)
+                                tvPreces.text = resources.getString(R.string.precesCt)
+
+                            }
+                        })
+                    } catch (e: InterruptedException) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace()
+                    }
+                }).run()
+            }
+            else -> {}
+        }
+
+        return true
+    }*/
 }
